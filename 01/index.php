d6ae719dd8f329dd8261a4b41b53cc0abc864c43
Antony CASTANER <antony.castaner@ynov.com>
2022-12-01 10:10:08 +0100

    Set 01

---
+++ 01/index.php
@@ -0,0 +1,32 @@
+<?php
+
+// Part One
+$inputFile = fopen('input.txt', 'r');
+$allCalories = array();
+$currentCalories = 0;
+$currentLine = null;
+
+while (!feof($inputFile)) {
+  $currentLine = fgets($inputFile);
+  if (empty(trim($currentLine))) {
+    // Insert each Elve
+    array_push($allCalories, $currentCalories);
+    $currentCalories = 0;
+  } else {
+    // Increment each Elve
+    $currentCalories += (int) $currentLine;
+  }
+}
+// Insert last Elve
+array_push($allCalories, $currentCalories);
+
+echo "Part One: " . max($allCalories) . "\n";
+
+// Part Two
+rsort($allCalories);
+$countTopThree = 0;
+for ($i = 0; $i < 3; $i++) {
+  $countTopThree += $allCalories[$i];
+}
+
+echo "Part Two: " . $countTopThree . "\n";

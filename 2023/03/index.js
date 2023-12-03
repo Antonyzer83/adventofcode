53fe4f5ad1555aba89d94336756547105c837211
Antony CASTANER <antony.castaner@ynov.com>
2023-12-03 15:24:42 +0100

    feat: Set 3 part two

--- 2023/03/index.js
+++ 2023/03/index.js
@@ -57,3 +57,93 @@ content.split("\n").forEach((line) => {
 });
 
 console.log("Part One ", sum);
+
+row = 0;
+sum = 0;
+const positions = [];
+
+content.split("\n").forEach((line) => {
+  const numbers = line.matchAll(/[0-9][0-9]*/g);
+
+  // loop over regex object
+  for (const number of numbers) {
+    let index = number.index;
+
+    number[0].split("").forEach(() => {
+      let x = undefined;
+      let y = undefined;
+
+      if (index > 0 && /\*/.test(table[row][index - 1])) {
+        x = index - 1;
+        y = row;
+      } else if (
+        index < table[row].length - 1 &&
+        /\*/.test(table[row][index + 1])
+      ) {
+        x = index + 1;
+        y = row;
+      } else if (row > 0 && /\*/.test(table[row - 1][index])) {
+        x = index;
+        y = row - 1;
+      } else if (row < table.length - 1 && /\*/.test(table[row + 1][index])) {
+        x = index;
+        y = row + 1;
+      } else if (row > 0 && index > 0 && /\*/.test(table[row - 1][index - 1])) {
+        x = index - 1;
+        y = row - 1;
+      } else if (
+        row > 0 &&
+        index < table[row].length - 1 &&
+        /\*/.test(table[row - 1][index + 1])
+      ) {
+        x = index + 1;
+        y = row - 1;
+      } else if (
+        row < table.length - 1 &&
+        index > 0 &&
+        /\*/.test(table[row + 1][index - 1])
+      ) {
+        x = index - 1;
+        y = row + 1;
+      } else if (
+        row < table.length - 1 &&
+        index < table[row].length - 1 &&
+        /\*/.test(table[row + 1][index + 1])
+      ) {
+        x = index + 1;
+        y = row + 1;
+      }
+
+      if (
+        x !== undefined &&
+        y !== undefined &&
+        positions.find(
+          (p) => p.number === number[0] && p.x === x && p.y === y
+        ) === undefined
+      ) {
+        positions.push({
+          x,
+          y,
+          number: number[0],
+        });
+      }
+
+      index++;
+    });
+  }
+
+  row++;
+});
+
+for (position of positions) {
+  const secondPosition = positions.find(
+    (p) =>
+      p.x === position.x && p.y === position.y && p.number !== position.number
+  );
+
+  if (secondPosition) {
+    sum += (position.number * secondPosition.number) / 2;
+  }
+}
+
+console.log("Part Two ", sum);

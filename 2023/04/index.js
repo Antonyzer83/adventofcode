19503e5e68e64b3a8459c1d7f2c5f67527408147
Antony CASTANER <antony.castaner@ynov.com>
2023-12-04 23:15:15 +0100

    feat: Set 4 part two

--- 2023/04/index.js
+++ 2023/04/index.js
@@ -33,4 +33,50 @@ content.split("\n").forEach((line) => {
   total += count;
 });
 
-console.log("Part Two ", total);
+console.log("Part One ", total);
+
+const ids = {};
+
+content.split("\n").forEach((line) => {
+  const [match, list] = line.split(":");
+  const id = parseInt(match.split(" ").filter((num) => num.length)[1]);
+  const lists = list.split("|");
+
+  const winNums = lists[0]
+    .split(" ")
+    .filter((num) => num.length)
+    .map((num) => parseInt(num));
+  const nums = lists[1]
+    .split(" ")
+    .filter((num) => num.length)
+    .map((num) => parseInt(num));
+
+  let count = 0;
+
+  winNums.forEach((num) => {
+    if (nums.includes(num)) {
+      count++;
+    }
+  });
+
+  // Set default value for current match
+  if (!ids[id]) {
+    ids[id] = 1;
+  }
+
+  // Create copy of following matches
+  for (let i = id + 1; i < id + 1 + count; i++) {
+    if (!ids[i]) {
+      ids[i] = 1;
+    }
+
+    ids[i] = ids[i] + 1 * ids[id];
+  }
+});
+
+console.log(
+  "Part Two ",
+  Object.values(ids)
+    .map((id) => id)
+    .reduce((a, b) => a + b, 0)
+);
--- 2023/README.md
+++ 2023/README.md
@@ -6,3 +6,18 @@
 
 - Part One: Concat the first and last number of each row and sum them up.
 - Part Two: Same but with number in word form.
+
+## 02
+
+- Part One: check the game that respects available colors max
+- Part Two: recover max colors
+
+## 03
+
+- Part One: find part numbers and sum them up
+- Part Two: multiply part numbers with gear between them
+
+## 04
+
+- Part One: calculate card points with winning numbers
+- Part Two: count copies of card

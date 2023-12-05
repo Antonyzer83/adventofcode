f691d2bfe9a2677fb932dbf1420c9f983a6390a1
Antony CASTANER <antony.castaner@ynov.com>
2023-12-05 18:43:51 +0100

    feat: Set 5 part one

---
+++ 2023/5/index.js
@@ -0,0 +1,45 @@
+const fs = require("node:fs");
+
+// const content = fs.readFileSync("./input-example.txt", "utf8");
+const content = fs.readFileSync("./input.txt", "utf8");
+
+const maps = {};
+let currentStep = "seed";
+let previousStep = "";
+let indexes = [];
+
+content.split("\n").forEach((line, index) => {
+  if (index === 0) {
+    maps["seed"] = line.split(":")[1].trim().split(" ").map(Number);
+  } else {
+    if (line.length) {
+      // Init step data
+      if (line.includes("map")) {
+        currentStep = line.split(" ")[0].split("-")[2];
+        maps[currentStep] = maps[previousStep];
+      } else {
+        const [dst, src, range] = line.split(" ").map(Number);
+
+        maps[currentStep].forEach((value, index) => {
+          if (
+            value >= src &&
+            value <= src + range &&
+            indexes.indexOf(index) === -1
+          ) {
+            maps[currentStep][index] = dst + (value - src);
+            indexes.push(index);
+          }
+        });
+      }
+    } else {
+      // Reset step
+      console.log(currentStep, maps[currentStep]);
+      previousStep = currentStep;
+      currentStep = "";
+      indexes = [];
+    }
+  }
+});
+
+console.log(maps);
+console.log("Part One ", Math.min.apply(Math, Object.values(maps).pop()));
